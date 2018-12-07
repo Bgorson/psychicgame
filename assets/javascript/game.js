b6285129e677065c5bc7d670a76f8f905d4c5835
Brandon Gorson <42119471+Bgorson@users.noreply.github.com>
2018-12-06 19:51:01 -0600

    Minor Logic fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,10 +22,10 @@ var guessText = document.getElementById("guesstext");
 var userGuessText = document.getElementById("userguesstext");
 var guessesLeftText= document.getElementById("guessesleft");
 var tryAgainText= document.getElementById("tryagain");
-
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //Function to randomly select a letter
 function randLetter() {
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var letter = letters[Math.floor(Math.random() * letters.length)];
     return letter
 };
@@ -36,6 +36,8 @@ function newGame() {
     chosenLetter = randLetter();
     guessesArray = [];
     guessesLeft = 10;
+    chosenLetter;
+    guessText.textContent= "";
     tryAgainText.textContent= "";
     }
 
@@ -43,29 +45,37 @@ function newGame() {
 document.onkeyup = function(event) {
    var userGuess = event.key;
    //if user is wrong and is out of guesses
-   if (guessesArray.includes(userGuess)) {
-    tryAgainText.textContent = "you did that already, pick again"
-   }
-   else if (guessesLeft == 0) {
-    loses++;
-    newGame();
-}
-   else if (userGuess !== chosenLetter) {
-    guessesLeft--;
-    guessesArray.push(userGuess);
-    tryAgainText.textContent= "";
+if (letters.includes(userGuess)){
+        if (guessesArray.includes(userGuess)) {
+            tryAgainText.textContent = "you did that already, pick again"
+        }
+        if (guessesLeft == 0) {
+            loses++;
+            guessText.textContent= "";
+            newGame();
+        }
+        else if (userGuess !== chosenLetter) {
+            guessesLeft--;
+            guessesArray.push(userGuess);
+            tryAgainText.textContent= "";
+            }
+        
+            //if user is correct
+            else if (userGuess == chosenLetter) {
+                wins++;
+                guessText.textContent= "";
+                newGame();
+            };
+        //Display scoreboard
+            guessText.textContent = "You just guessed: " + userGuess;
+            winText.textContent = "wins: " + wins;
+            loseText.textContent = "losses: " + loses;
+            userGuessText.textContent = "You've already chosen: " + guessesArray;
+            guessesLeftText.textContent= "You've got " + guessesLeft +" chances left!"
+        }
+    
+    else {
+        tryAgainText.textContent = "You can only choose lowercase letters"
     }
-   
-    //if user is correct
-    else if (userGuess == chosenLetter) {
-        wins++;
-        newGame();
-    };
-//Display scoreboard
-    guessText.textContent = "You just guessed " + userGuess;
-    winText.textContent = "wins: " + wins;
-    loseText.textContent = "losses: " + loses;
-    userGuessText.textContent = "You've already chosen " + guessesArray;
-    guessesLeftText.textContent= "You've got " + guessesLeft +" chances left!"
 }
 newGame()
